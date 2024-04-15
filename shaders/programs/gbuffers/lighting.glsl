1c663474d20c04cd07619d227ee0445225d5861e
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-04-14 20:07:35 -0700

    breaks the compiler

--- shaders/programs/gbuffers/lighting.glsl
+++ shaders/programs/gbuffers/lighting.glsl
@@ -11,11 +11,13 @@ uniform vec3 moonPosition;
 varying vec2 lm_coords;
 varying vec2 tex_coords;
 varying vec4 frag_color;
+varying vec4 normal_vec;
 
 void main() {
 	vec4 color = texture2D(texture, tex_coords) * frag_color;
-	color *= texture2D(lightmap, lm_coords);
 	color.xyz = pow(color.xyz, vec3(GAMMA_MULTIPLIER));
+	color *= texture2D(lightmap, lm_coords).x;
+	color += vec3(1.0, 0.95, 0.9) * max(dot(normal_vec.xyz, normalize(sunPosition)), 0.0);
     color.xyz *= HDR_MULTIPLIER;
 
     /* DRAWBUFFERS:0 */
@@ -28,6 +30,7 @@ void main() {
 varying vec2 lm_coords;
 varying vec2 tex_coords;
 varying vec4 frag_color;
+varying vec4 normal_vec;
 
 uniform mat4 gbufferModelView;
 
@@ -36,6 +39,8 @@ void main() {
 	tex_coords = (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
 	lm_coords  = (gl_TextureMatrix[1] * gl_MultiTexCoord1).xy;
 	frag_color = gl_Color;
+	normal_vec.xyz = gl_Normal;
+	normal_vec.w = 0.0;
 }
 
 #endif
