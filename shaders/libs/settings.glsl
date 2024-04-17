490f2c8382bef73eb1fb5dc85dcc02396de2df97
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-04-16 21:11:32 -0700

    added shadow mapping

--- shaders/libs/settings.glsl
+++ shaders/libs/settings.glsl
@@ -5,5 +5,6 @@ const int colortex0Format = RGBA16F;
 const int colortex3Format = RGBA8_SNORM;
 */
 
+#define SHADOW_MAP_RES 1024  // Shadow Map Resolution [256 512 1024 2048 4096 8192]
 #define HDR_MULTIPLIER 1.7  // High Dynamic Range [1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0]
 #define GAMMA_MULTIPLIER 2.2  // Gamma [1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0]
