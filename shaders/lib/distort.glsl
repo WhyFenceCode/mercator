62efc13d8ab3727ef99d62540215a2eb04e441b2
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-09-13 18:49:37 -0700

    Update distort.glsl

--- shaders/lib/distort.glsl
+++ shaders/lib/distort.glsl
@@ -1,3 +1,7 @@
+const bool shadowtex0Nearest = true;
+const bool shadowtex1Nearest = true;
+const bool shadowcolor0Nearest = true;
+
 vec3 distortShadowClipPos(vec3 shadowClipPos){
   float distortionFactor = length(shadowClipPos.xy); // distance from the player in shadow clip space
   distortionFactor += 0.1; // very small distances can cause issues so we add this to slightly reduce the distortion
