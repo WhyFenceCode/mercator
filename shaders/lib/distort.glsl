6ca7f2f7f0eda47625f47773df1736fe03f88b8b
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-09-13 18:43:57 -0700

    Create distort.glsl

---
+++ shaders/lib/distort.glsl
@@ -0,0 +1,8 @@
+vec3 distortShadowClipPos(vec3 shadowClipPos){
+  float distortionFactor = length(shadowClipPos.xy); // distance from the player in shadow clip space
+  distortionFactor += 0.1; // very small distances can cause issues so we add this to slightly reduce the distortion
+
+  shadowClipPos.xy /= distortionFactor;
+  shadowClipPos.z *= 0.5; // increases shadow distance on the Z axis, which helps when the sun is very low in the sky
+  return shadowClipPos;
+}
